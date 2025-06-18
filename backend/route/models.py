0875ffe630b23a457c105118e45e852bdf1659b3
Juanctoro <juancarlostorocifuentes12@gmail.com>
2025-06-17 22:28:24 -0500

    fix: correct small errors

--- backend/route/models.py
+++ backend/route/models.py
@@ -1,9 +1,22 @@
+from django.contrib.gis.db import models as gis_models
 from django.db import models
+from trip.models import Trip
 
 class Route(models.Model):
+    trip             = models.ForeignKey(
+                           Trip,
+                           on_delete=models.CASCADE,
+                           related_name='routes',
+                           null=True
+                       )
     duration_minutes = models.PositiveIntegerField('Duration (minutes)')
     distance         = models.FloatField('Distance (km)')
-    stops            = models.PositiveIntegerField('Number of Stops')
+    stops            = gis_models.LineStringField('Stops Line', srid=4326)
 
-    def __str__(self):
-        return f"Route {self.id}: {self.distance} km in {self.duration_minutes} min ({self.stops} stops)"
+    def str(self):
+        # muestra el id del Trip y los detalles de la ruta
+        return (
+            f"Route {self.id} for Trip {self.trip.id}: "
+            f"{self.distance} km in {self.duration_minutes} min "
+            f"({self.stops.num_coords} puntos)"
+        )
--- backend/trip/models.py
+++ backend/trip/models.py
@@ -8,9 +8,12 @@ class Trip(models.Model):
         on_delete=models.CASCADE,
         related_name='trips'
     )
-    start_point = models.CharField('Start point', max_length=150)
-    end_point   = models.CharField('End Point', max_length=150)
+    start_point = gis_models.PointField('Start Point', srid=4326)
+    end_point   = gis_models.PointField('End Point',   srid=4326)
     qr_url      = models.URLField('QR Code URL', max_length=200, blank=True)
 
-    def __str__(self):
-        return f"Trip {self.id} for vehicle {self.vehicle.plate} from {self.start_point} to {self.end_point}"
+    def str(self):
+        return (
+            f"Trip {self.id} for vehicle {self.vehicle.plate} "
+            f"from {self.start_point.tuple} to {self.end_point.tuple}"
+        )
